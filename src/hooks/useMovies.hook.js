9d061d341349ff61b21bae078fdaf36ee23efa1e
Mateusz <m.wesolowski.92@gmail.com>
2023-09-04 23:31:12 +0200

    create fech movie hook

---
+++ src/hooks/useMovies.hook.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+const KEY = "8266c2c";
+
+export const useMovies = (query) => {
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		const controller = new AbortController();
+		const fetchMovies = async () => {
+			try {
+				setIsLoading(true);
+				setError("");
+				const res = await fetch(
+					`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+					{ signal: controller.signal }
+				);
+				const data = await res.json();
+
+				if (!res.ok)
+					throw new Error("Something went wrong with fetching movies");
+
+				if (data.Response === "False") throw new Error("Movie not found!");
+				setMovies(data.Search);
+				setError("");
+			} catch (err) {
+				if (err.name !== "AbortError") {
+					setError(err.message);
+				}
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		if (query.length < 3) {
+			setMovies([]);
+			setError("");
+			return;
+		}
+
+		// handleCloseMovie();
+		fetchMovies();
+
+		return () => {
+			controller.abort();
+		};
+	}, [query]);
+
+	return {
+		movies,
+		isLoading,
+		error,
+	};
+};
